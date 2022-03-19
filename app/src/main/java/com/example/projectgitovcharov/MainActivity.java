f103cfc3cc619b1dd2a4dac2699c7c91514b176d
Chumochka <danpro1010@bk.ru>
2022-03-19 15:46:25 +0700

    Комментарий 3 в MainActivity.java

--- app/src/main/java/com/example/projectgitovcharov/MainActivity.java
+++ app/src/main/java/com/example/projectgitovcharov/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //main
-    @Override
+    @Override//com3
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
