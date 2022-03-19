3f9304f6e3541a4e0c020dfa4a170dcca6a9f426
Chumochka <danpro1010@bk.ru>
2022-03-19 15:49:13 +0700

    Комментарий 8 в MainActivity.java

--- app/src/main/java/com/example/projectgitovcharov/MainActivity.java
+++ app/src/main/java/com/example/projectgitovcharov/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 //main
     @Override//com3
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//com8
         setContentView(R.layout.activity_main);
     }//com5
 }
